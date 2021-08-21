33dadab7d02a9f378347b6f5c74c76a451edd0ce
nazaawa <yetshandedi@gmail>
2021-08-21 16:19:10 +0100

    ll project

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-     
+        primaryColorBrightness: Brightness.light,
       ),
       home: MyHomePage(),
     );
